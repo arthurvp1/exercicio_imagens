0ac4e83d4b1738d4adfd5b2b2cbe276d966fb446
ARTHUR VIECILI PINHEIRO DA SILVA <arthur_vp_silva@estudante.sesisenai.org.br>
2025-02-18 16:47:46 -0300

    a

--- Aula2/vite.config.js
+++ Aula2/vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'exercicioimagem',
+        short_name: 'imgs',
+        description: 'exercicio gerador de imagem',
+        theme_color: '#fdf8f9',
+        icons: [
+          {
+            src: 'socoinlges.svg',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'bottle.svg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
